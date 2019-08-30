77f97f754a28a4d54d794c71637da6e63dada800
darrenbates96 <i.am.darrenbates@gmail.com>
2019-08-30 11:26:00 +0200

    added to my section

--- test.R
+++ test.R
@@ -33,7 +33,7 @@ KNOK.cor
 plot(KNOK.cor, xlim=c(-0.25, 0.4))
 
 nl<-netlm(KNOKE.n,           # Dependent variable/network
-          KNOKI.n, # List the independent variables/networks
+          KNOKI.n,           # List the independent variables/networks
           reps=1000)  
 summary(nl)
 
@@ -51,3 +51,28 @@ KNOK.group = factor(rep(letters[1:2], each = 100))
 fit = lm(formula = KNOK.v ~ KNOK.group)
 
 cug.test(KNOKE.n, cmode="size")
+
+#### Darren Trying Something ####
+
+#Creating a network coding each element as
+#"1" if both are private or both are non-private, 
+#and a "0" if they are of mixed types.
+
+KNOKP.n <-  matrix(
+  c(0,1,0,1,0,0,0,1,0,1,1,0,0,1,0,0,0,1,0,1,0,0,0,0,1,1,1,0,1,0,1,1,0,0,0,0,0,1,0,1,0,0,1,0,0,1,1,0,1,0,0,0,1,0,1,0,1,0,1,0,0,0,1,0,1,1,0,0,1,0,1,1,0,1,0,0,0,0,0,1,0,0,1,0,1,1,1,0,0,0,1,1,0,1,0,0,0,1,0,0),
+  nrow=10,
+  ncol=10,
+  byrow=T)
+
+dimnames(KNOKP.n) = list(c(1,2,3,4,5,6,7,8,9,10), c("C","C","E","I","M","W","N","U","W","W"))
+
+#Performance of Standard Multiple Regression 
+#analysis by regressing each element in the 
+#network on its corresponding elements in the 
+#information network
+
+nl_p<-netlm(KNOKI.n,           
+            KNOKP.n,             
+            reps=1000)  
+summary(nl_p)
+
