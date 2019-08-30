16367e44d7d3323af8f3c01925ea75ebda5275b8
DavenMauff <dsmauff@gmail.com>
2019-08-30 11:20:29 +0200

    aldu's changes

--- test.R
+++ test.R
@@ -1,4 +1,7 @@
 library(statnet)
+library(resample)
+library(snowboot)
+library(igraph)
 
 KNOKI <-  matrix(
   c(0,1,0,0,1,0,1,0,1,0,1,0,1,1,1,0,1,1,1,0,0,1,0,1,1,1,1,0,0,1,1,1,0,0,1,0,1,0,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,0,0,0,1,0,1,0,0,1,0,1,1,0,0,0,0,0,1,1,0,1,1,0,1,0,1,0,0,1,0,0,1,0,1,0,0,0,1,1,1,0,1,0,1,0,0,0),
@@ -24,11 +27,28 @@ KNOKE.n
 summary(KNOKI.n)
 summary(KNOKE.n)
 
+
+#In this instance, Binary Networks use density for a degree of central tendency
+
+#When creating predicitions of network densities, creating the hypothesus around an expected density is necessary and then to measure vairation around the expected and actual density
 network.density(KNOKI.n)
 network.density(KNOKE.n)
 
+set.seed(1)
+B <- 500
+KNOKI.boot <- vertboot(KNOKI, B)
+
+densities <- sapply(1:B, function)
+
+
+KNOK.bootstrap <- bootstrap(KNOKI)
+network.density(KNOK.bootstrap)
+
 KNOK.cor <- qaptest(list(KNOKE, KNOKI), gcor, g1=1, g2=2, reps=2000)
 KNOK.cor
+
+KNOK.den <- qaptest(KNOKE, density, reps=1000)
+KNOK.den
 
 plot(KNOK.cor, xlim=c(-0.25, 0.4))
 
@@ -40,14 +60,14 @@ summary(nl)
 nlog<-netlogit(KNOKE.n, KNOKI.n,reps=1000)
 summary(nlog)
 
-KNOK.a <- aov
+#KNOK.a <- aov
 
 gcor(KNOKI, KNOKE)
 
-KNOKE.v <- as.vector(KNOKE.n)
-KNOKI.v <- as.vector(KNOKI.n)
-KNOK.v <- c(KNOKE.v, KNOKI.v)
-KNOK.group = factor(rep(letters[1:2], each = 100))
-fit = lm(formula = KNOK.v ~ KNOK.group)
+#KNOKE.v <- as.vector(KNOKE.n)
+#KNOKI.v <- as.vector(KNOKI.n)
+#KNOK.v <- c(KNOKE.v, KNOKI.v)
+#KNOK.group = factor(rep(letters[1:2], each = 100))
+#fit = lm(formula = KNOK.v ~ KNOK.group)
 
-cug.test(KNOKE.n, cmode="size")
+#cug.test(KNOKE.n, cmode="size")
